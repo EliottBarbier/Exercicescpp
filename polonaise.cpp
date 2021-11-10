4b084f3feebfe1cc77710ab243f7b08dfb101ad8
Eliott <eliott.barbier@mines-paristech.fr>
2021-11-10 14:33:52 +0100

    Tests de défnition de fonctions

--- polonaise.cpp
+++ polonaise.cpp
@@ -2,6 +2,37 @@
 #include <string>
 #include <vector>
 
+//float operation(std::vector<float>& vect, const int i ){
+//    std::array<string, 4> liste
+//  list[0]="+"
+//    list[1]="-"
+//    list[2]="/"
+//    list[3]="x"
+//    std::string a = argv[i]
+//    int ope = -1
+//    if(a=="+"){
+//        ope=0
+//    }
+//    else if(a=="-"){
+//        ope=1
+//    }
+//    else if(a=="/"){
+//        ope=2
+//    }
+//    else if(a=="x"){
+//        ope=3
+//    }
+//    else{
+//        vect.push_back(std::stof(a))
+//    }
+//    float b = vect.back()
+//    vect.pop_back()
+//    float c = vect.back()
+//    vect.pop_back()
+//    vect.push_back
+//
+//}  
+
 int main(int argc, char** argv){
     std::vector<std::string> v {"+","-","/","x"};
     std::cout << v.at(0) << std::endl;
